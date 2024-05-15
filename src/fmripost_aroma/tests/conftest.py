0dc05b32b5c475608c58ab0c532dd4bb181d9b5d
Chris Markiewicz <effigies@gmail.com>
2024-05-15 14:34:00 -0400

    TEST: Get tests working

--- src/fmripost_aroma/tests/conftest.py
+++ src/fmripost_aroma/tests/conftest.py
@@ -1,63 +1,34 @@
 """Fixtures for the CircleCI tests."""
 
 import base64
+import importlib.resources
 import os
 
 import pytest
 
 
-def pytest_addoption(parser):
-    """Collect pytest parameters for running tests."""
-    parser.addoption(
-        '--data_dir',
-        action='store',
-        default='/home/runner/work/fmripost-aroma/fmripost-aroma/src/fmripost_aroma/tests/data',
-    )
-    parser.addoption(
-        '--output_dir',
-        action='store',
-        default='/home/runner/work/pytests/out',
-    )
-    parser.addoption(
-        '--working_dir',
-        action='store',
-        default='/home/runner/work/pytests/work',
-    )
-
-
 # Set up the commandline options as fixtures
 @pytest.fixture(scope='session')
-def data_dir(request):
+def data_dir():
     """Grab data directory."""
-    return request.config.getoption('--data_dir')
-
-
-@pytest.fixture(scope='session')
-def working_dir(request):
-    """Grab working directory."""
-    workdir = request.config.getoption('--working_dir')
-    os.makedirs(workdir, exist_ok=True)
-    return workdir
-
-
-@pytest.fixture(scope='session')
-def output_dir(request):
-    """Grab output directory."""
-    outdir = request.config.getoption('--output_dir')
-    os.makedirs(outdir, exist_ok=True)
-    return outdir
+    test_data = importlib.resources.files('fmripost_aroma.tests') / 'data'
+    with importlib.resources.as_file(test_data) as data:
+        yield data
 
 
 @pytest.fixture(scope='session', autouse=True)
-def _fslicense(working_dir):
+def _fslicense(tmp_path_factory):
     """Set the FreeSurfer license as an environment variable."""
+    working_dir = tmp_path_factory.mktemp('fslicense')
     FS_LICENSE = os.path.join(working_dir, 'license.txt')
-    os.environ['FS_LICENSE'] = FS_LICENSE
     LICENSE_CODE = (
         'bWF0dGhldy5jaWVzbGFrQHBzeWNoLnVjc2IuZWR1CjIwNzA2CipDZmVWZEg1VVQ4clkKRlNCWVouVWtlVElDdwo='
     )
     with open(FS_LICENSE, 'w') as f:
         f.write(base64.b64decode(LICENSE_CODE).decode())
+
+    os.putenv('FS_LICENSE', FS_LICENSE)
+    return
 
 
 @pytest.fixture(scope='session')
